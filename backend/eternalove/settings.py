2f9a85ccf2914cebbe7a27d291ed463de802df3e
Milos Popovic <misa01523@gmail.com>
2024-12-01 17:49:10 +0100

    Updated database location

--- backend/eternalove/settings.py
+++ backend/eternalove/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'eternalove.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env('DATABASE_NAME', default=BASE_DIR / 'db.sqlite3'),
     }
 }
 
